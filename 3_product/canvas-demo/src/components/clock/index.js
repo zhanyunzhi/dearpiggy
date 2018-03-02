dc115ced02f4568af3b45632c3e754754eb9b13c
zhanyunzhi <540562332@qq.com>
2018-03-02 17:41:02 +0800

    还没做适配

--- 3_product/canvas-demo/src/components/clock/index.js
+++ 3_product/canvas-demo/src/components/clock/index.js
@@ -6,18 +6,20 @@ function index(){
     $(function(){
         var canvas = document.getElementById("canvas");
         var ctx = canvas.getContext("2d");
-        var canvasWidth = 200;
-        var canvasHeight = 200;
+        var canvasWidth = 300;
+        var canvasHeight = 300;
         canvas.width = canvasWidth;
         canvas.height = canvasHeight;
 
         var r = canvasWidth / 2;
-        ctx.translate(r,r);         //重新映射画布上的（0,0）位置，这里将（0,0）位置移动到（r,r)位置
+        var rem = canvasWidth / 200;            //缩放比例
 
         function drawCircle(radius){
-            ctx.lineWidth = 10;
+            ctx.save();
+            ctx.translate(r,r);         //重新映射画布上的（0,0）位置，这里将（0,0）位置移动到（r,r)位置
+            ctx.lineWidth = 10 * rem;
             ctx.beginPath();
-            ctx.arc(0, 0, radius - ctx.lineWidth /2, 0, 2 * Math.PI, false);       //因为起始位置已经移动，所以圆心是0,0
+            ctx.arc(0, 0, radius - ctx.lineWidth / 2, 0, 2 * Math.PI, false);       //因为起始位置已经移动，所以圆心是0,0
             ctx.stroke();
         }
         function drawNumber(radius){
@@ -25,7 +27,7 @@ function index(){
             var x = 0;      //坐标X
             var y = 0;      //坐标Y
             var rad = 0;        //弧度
-            ctx.font = '18px';      //设置字体大小
+            ctx.font = 18 * rem + 'px';      //设置字体大小
             ctx.textAlign = 'center';       //设置文本内容的当前对齐方式
             ctx.textBaseline = 'middle';        //设置绘制文本时使用的当前文本基线
             num.forEach(function(number, i){
@@ -49,12 +51,69 @@ function index(){
                 ctx.fill();
             }
         }
+        function drawHour(hour, minute){
+            ctx.save();     //保存当前环境状态
+            ctx.beginPath();
+            var rad = 2 * Math.PI / 12 * hour;      //旋转的弧度，根据参数计算
+            var mrad = 2 * Math.PI / 12 / 60 * minute;      //旋转的弧度，根据参数计算分钟数引起的时针弧度
+            ctx.rotate(rad + mrad);
+            ctx.lineWidth = 6;          //设置线的宽度
+            ctx.lineCap = 'round';      //设置线的结束端点样式
+            ctx.moveTo(0, 10);          //移动到初始位置
+            ctx.lineTo(0, -r / 2);      //移动到目标位置
+            ctx.stroke();               //画线
+            ctx.restore();      //返回之前保存过的路径状态和属性
+        }
+        function drawMinute(minute){
+            ctx.save();     //保存当前环境状态
+            ctx.beginPath();
+            var rad = 2 * Math.PI / 60 * minute;      //旋转的弧度，根据参数计算
+            ctx.rotate(rad);
+            ctx.lineWidth = 3;          //设置线的宽度
+            ctx.lineCap = 'round';      //设置线的结束端点样式
+            ctx.moveTo(0, 10);          //移动到初始位置
+            ctx.lineTo(0, -r / 2 - 20);      //移动到目标位置
+            ctx.stroke();               //画线
+            ctx.restore();      //返回之前保存过的路径状态和属性
+        }
+        function drawSecond(second){
+            ctx.save();     //保存当前环境状态
+            ctx.beginPath();
+            ctx.fillStyle = '#c14543';
+            var rad = 2 * Math.PI / 60 * second;      //旋转的弧度，根据参数计算
+            ctx.rotate(rad);
+            ctx.moveTo(-2, 20);          //移动到初始位置
+            ctx.lineTo(2, 20);      //移动到目标位置
+            ctx.lineTo(1, -r / 2 - 30);      //移动到目标位置
+            ctx.lineTo(-1, -r / 2 - 30);      //移动到目标位置
+            ctx.fill();               //画线
+            ctx.restore();      //返回之前保存过的路径状态和属性
+        }
+        function drawDot(){
+            ctx.save();     //保存当前环境状态
+            ctx.beginPath();
+            ctx.fillStyle = '#ffffff';
+            ctx.arc(0, 0, 2, 0, Math.PI * 2, false);
+            ctx.fill();               //画线
+            ctx.restore();      //返回之前保存过的路径状态和属性
+        }
 
         function init(){
+            ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+            var now = new Date();
+            var hour = now.getHours();
+            var minute = now.getMinutes();
+            var second = now.getSeconds();
             drawCircle(r);
             drawNumber(r);
+            drawHour(hour,minute);
+            drawMinute(minute);
+            drawSecond(second);
+            drawDot();
+            ctx.restore();
         }
         init();
+        setInterval(init,1000)
     })
 
     return {
